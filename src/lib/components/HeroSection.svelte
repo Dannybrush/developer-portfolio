0a5dd1d42eef95810d8771d7eebedb46e84a3010
Daniel Broomhead <Dannyb0903@gmail.com>
2025-03-26 10:46:37 +0000

    add hero section, setup files & aliases

---
+++ src/lib/components/HeroSection.svelte
@@ -0,0 +1,9 @@
+<section class="hero-section default-margin"> 
+<h3>
+ Hi I'm Daniel Broomhead
+</h3>
+<div class="underscore mb-m"> </div>
+
+<h1> I'm a Svelte Developer crafting intuitive and performant</h1>
+<h1 class="dark-grey">web experiences.</h1>
+</section>
