9bdbf3eb4e6eef200c7f04c93aef435d61e0a9c4
Daniel Broomhead <Dannyb0903@gmail.com>
2025-03-26 10:52:19 +0000

    add button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import type { Snippet } from "svelte";
+
+
+     interface ButtonProps{
+        children: Snippet
+     }
+
+     let {children}: ButtonProps = $props();
+</script>
+
+<button> 
+    {@render children()}
+</button>
